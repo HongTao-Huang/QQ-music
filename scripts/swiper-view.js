ac9908fc1d10771f1e4360093377dbb12ce768fc
huanght <hht51770@hotmail.com>
2018-06-08 00:02:45 +0800

    header&slide

---
+++ scripts/swiper-view.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay:true,
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+})
