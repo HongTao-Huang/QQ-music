1c8fdd1cc1b0fd5ae4c174f3f4bd35a209c9785b
huanght <hht51770@hotmail.com>
2018-06-14 09:44:51 +0800

    添加排行榜及懒加载

---
+++ scripts/rank.js
@@ -0,0 +1,26 @@
+fetch('../json/ranking.json').then(respons => respons.json()).then(render);
+
+function render(json){
+    renderTopList(json.data.topList);
+    lazyload();
+}
+
+function renderTopList(tops) {
+    document.querySelector('#rankingView .topList').innerHTML = tops.map(top =>
+        `<li class="topListItem clearFix">
+                <div class="topItemMedia">
+                    <img data-src="${top.picUrl}" class="lazyload">
+                </div>
+                <div class="topItemInfo">
+                    <div class="topContent">
+                        <h3 class="topTitle">${top.topTitle}</h3>
+                        ${songList(top.songList)}
+                    </div>
+                </div>
+            </li>`
+    ).join('');
+}
+
+function songList(songs) {
+    return songs.map((song , index) => `<p class="topText">${index+1}<span class="songName">${song.songname}</span>- ${song.singername}</p>`).join('');
+}
